2b633efd9c5f9d67570d6f0a316e57221f576281
p3cet <cprzem@gmail.com>
2020-05-13 16:19:35 +0200

    joystick control added

---
+++ main.pas
@@ -0,0 +1,103 @@
+program nsg; //new speccy game
+//graphics 8 narrow screen 256x192
+
+uses atari, crt, dls, softPMG, joystick;
+
+Type  
+	State = (titleScreen, runGame, death, endGame, pauseGame);
+
+
+var
+	st: State;
+
+begin
+	Poke(709,12); // gr.8 luminance	
+	Poke(710,4);  // gr.8 background color
+
+	dlLvl1;
+	sdmctl:=33;	//narrow screen
+	genLineAddr(dls.vmem,dls.vmem2);
+	genTiles;
+{	putTile(0,0,0);
+	putTile(1,0,1);
+	shiftRight(0);
+	readkey;
+
+	shiftRight(1);
+	fillTileRight(0,1);
+	readkey;
+	pause;
+
+	shiftRight(2);
+	fillTileRight(1,1);
+	readkey;
+	pause;
+	
+	shiftRight(3);
+	fillTileRight(2,1);
+	readkey;
+	pause;}
+	
+	repeat
+		softPMG.movePlayer;
+		pause;
+	until keypressed;
+	
+	dlTitle;
+	st:=titleScreen;
+	
+	{main loop}
+	repeat
+	case st of
+		runGame:	begin;
+						if strig0=0 then begin
+							dlTitle;
+							st:=titleScreen;
+							Poke(strig0,1);
+						end;
+					end;
+		titleScreen: begin;
+						 writeln('ASG');
+						 writeln('Another Speccy Game');
+						 writeln('Press fire to start');
+						 if strig0=0 then begin
+							dlLvl1;
+							st:=runGame;
+							Poke(strig0,1);
+						end;
+					end;
+		death:		begin;
+					end;
+		endGame:	begin;
+					end;
+		pauseGame:	begin;
+					end;
+	end;
+	until keypressed;
+
+end.
+
+//procedure Dli2; interrupt;
+//begin
+//	asm { phr
+//	sta ATARI.WSYNC};
+{	colpm0:=$5;
+	colpm1:=$2;
+	colpm2:=$3;
+	colpm3:=$4;
+	asm {
+	plr };
+//end;
+
+
+//procedure Dli; interrupt;
+//begin
+//	asm { phr
+//	sta ATARI.WSYNC};
+{	colpm0:=$16;
+	colpm1:=$26;
+	colpm2:=$36;
+	colpm3:=$46;
+	asm { mwa #DLI2 ATARI.VDSLST
+	plr };
+//end;
---
+++ softPMG.pas
@@ -0,0 +1,184 @@
+unit softPMG;
+	//narrow screen 32 cells in the row
+	
+interface
+uses atari, joystick;
+	
+type
+	level = array [0..2] of byte;
+	spriteCol = array [0..15] of byte;
+	//spriteColPointer = ^spriteCol; not needed? just make a pointer to spriteCol
+	playerState = (run, jump, shoot, melee, dead);
+//	vertDir = (noneV, up, down);
+//	horDir = (noneH, left, right);
+	//tile = array [0..8] of byte;
+	
+const
+	player: array [0..7] of byte = (255,255,255,255,255,255,255,255);
+	level1: level =(0,1,2);
+
+var
+	offset: word = 0;
+	currVmem: word;
+	i: byte;
+	lineAddr: array [0..199] of word;
+	tileSet: array [0..23] of byte;		//new tile starts every 8th byte
+	
+	//--PLAYER VARS--//
+//	plVertDir: vertDir = noneV;
+//	plHorDir: horDir = noneH;
+	plByteX: byte = 0;			//plByteX and plByteY - position of left upper corner of the player sprite
+	plByteY: byte = 0;
+	plOffsetX: byte = 0;	//0 is default pos - add one to move right, sub 1 to move left, reset on 255 and 8
+	plOffsetY: byte = 0;	//0 is default pos - add one to move down, sub 1 to move up, reset on 255 and 8
+	//TODO: albo cos na zasadzie direction up right left down i kalkulacja offsetów na podstawie plByteX i plByteY
+	
+	fillMaskRight: array [0..7] of byte =(127,63,31,15,7,3,1,0);
+	fillMaskLeft: array [0..7] of byte = (254,252,248,240,224,192,128,0);
+	tempMask: byte =0;
+	cell: byte;
+	cellX: byte = 0;			//screen is logicaly divided for cells 8x8 pixels
+	cellY: byte = 0;			// X and Y gives number of cell horizontaly and verticaly
+	
+	shift: byte;
+	
+	//joystick
+	joyOffsetX: array [0..15] of smallint = (0,0,0,0,0,1,1,1,0,-1,-1,-1,0,0,0,0);
+	joyOffsetY: array [0..15] of smallint = (0,0,0,0,0,1,-1,0,0,1,-1,0,0,1,-1,0);
+	joyVal: byte;
+	joyOffset: smallint;
+	
+
+procedure movePlayer;	
+procedure shiftRight (x: byte);
+procedure shiftLeft (x: byte);
+procedure genLineAddr (vm1: word; vm2: word);
+procedure genTiles;
+procedure putTile(bx: byte; by: byte; tileNum: byte);
+procedure fillTileRight(x: byte; tileNum: byte);
+procedure fillTileLeft(x: byte; tileNum: byte);
+procedure processJoy;
+
+	
+implementation
+
+	procedure genLineAddr (vm1: word; vm2: word);
+	begin
+		lineAddr[0]:=vm1;
+		for i:=1 to 99 do begin
+			lineAddr[i]:=lineAddr[i-1]+32;		//wide screen 32 bytes *8 points per byte = 256 points
+		end;
+		lineAddr[100]:=vm2;
+		for i:=101 to 199 do begin
+			lineAddr[i]:=lineAddr[i-1]+32;		//wide screen 32 bytes *8 points per byte = 256 points
+		end;
+	end;
+	
+	procedure genTiles;
+	begin
+		tileSet[0]:=170;
+		tileSet[1]:=170;
+		tileSet[2]:=170;
+		tileSet[3]:=170;
+		tileSet[4]:=170;
+		tileSet[5]:=170;
+		tileSet[6]:=170;
+		tileSet[7]:=170;
+		tileSet[8]:=85;
+		tileSet[9]:=85;
+		tileSet[10]:=85;
+		tileSet[11]:=85;
+		tileSet[12]:=85;
+		tileSet[13]:=85;
+		tileSet[14]:=85;
+		tileSet[15]:=85;
+		tileSet[16]:=255;
+		tileSet[17]:=255;
+		tileSet[18]:=255;
+		tileSet[19]:=255;
+		tileSet[20]:=255;
+		tileSet[21]:=255;
+		tileSet[22]:=255;
+		tileSet[23]:=255;
+	end;
+	
+	procedure shiftRight (x: byte);		//TODO: doesnt work for zero shift (x=0)
+	begin
+		for i:=0 to 7 do begin
+			Poke(lineAddr[plByteY+i]+plByteX, player[i] shr x);
+			Poke(lineAddr[plByteY+i]+plByteX+1, player[i] shl (8-x));
+		end;
+	end;
+
+	procedure shiftLeft (x: byte);
+	begin
+		for i:=0 to 7 do begin
+			Poke(lineAddr[plByteY+i]+plByteX, player[i] shl x);
+			Poke(lineAddr[plByteY+i]+plByteX-1, player[i] shr (8-x));
+		end;
+	end;
+
+	procedure putTile(bx: byte; by: byte; tileNum: byte);
+	begin
+		tileNum:=tileNum*8;
+		for i:=0 to 7 do
+			Poke(lineAddr[by+i]+bx,tileSet[tileNum+i]);
+	end;
+	
+	procedure fillTileRight(x: byte; tileNum: byte);
+	begin
+		tileNum:=tileNum*8;
+		for i:=0 to 7 do begin
+			tempMask:=tileSet[tileNum+i] AND fillMaskRight[x];	//clear last x bits of the tile
+			cell:= Peek(lineAddr[plByteY+i]+plByteX+1) OR tempMask; //mix mask and the part of the sprite written in the byte
+			Poke(lineAddr[plByteY+i]+plByteX+1,cell);	 //add one to bx, because cell on the RIGHT side has to be filled
+		end;
+	end;
+
+	procedure fillTileLeft(x: byte; tileNum: byte);		//TODO: copy pasted from fillTileRight, if something goes wrong check it first
+	begin
+		tileNum:=tileNum*8;
+		for i:=0 to 7 do begin
+			tempMask:=tileSet[tileNum+i] AND fillMaskRight[x];	//clear first x bits of the tile
+			cell:= Peek(lineAddr[plByteY+i]+plByteX-1) OR tempMask; //mix mask and the part of the sprite written in the byte
+			Poke(lineAddr[plByteY+i]+plByteX-1,cell);	 //sub one from bx, because cell on the LEFT side has to be filled
+		end;
+	end;
+
+procedure processJoy;
+	begin
+		joyVal:=stick0;	 
+		joyOffset:=joyOffsetX[joyVal];
+		//TODO: check if move is possible
+		
+		//update player position
+		plOffsetX:=plOffsetX+joyOffset;		//TODO: check - add smallint to byte
+		if (plOffsetX=8) and (joyOffset=1) then
+		begin
+			plOffsetX:=0; //reset plOffsetX
+			inc(plByteX)
+		end
+		else if plOffsetX=255  then
+		begin
+			plOffsetX:=7; //reset plOffsetX
+			dec(plByteX);
+		end;
+		
+		joyOffset:=joyOffsetY[joyVal];
+		plOffsetY:=plOffsetY+joyOffset;
+		if (plOffsetY=255) then plOffsetY:=7
+		else if plOffsetY=8 then plOffsetY:=0; //reset plOffsetY 
+		plByteY:=plByteY+joyOffset;	//TODO: vmem change check!
+		
+	end;
+
+	procedure movePlayer;
+	begin
+		processJoy;
+		joyOffset:=joyOffsetX[joyVal];
+		if joyOffset=1 then shiftRight(plOffsetX)
+		else if joyOffset=-1 then shiftLeft(plOffsetX);
+	end;
+
+
+end.
